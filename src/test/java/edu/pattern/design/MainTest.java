9975b65c0e9a8e0cd4797055dbcd7d9add5863c2
YeongRoYun <colabofyyr@gmail.com>
2023-01-04 01:52:05 +0900

    Start studying

---
+++ src/test/java/edu/pattern/design/MainTest.java
@@ -0,0 +1,14 @@
+package edu.pattern.design;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    @DisplayName("hello")
+    void test() {
+
+    }
+}
